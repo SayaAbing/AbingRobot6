2e1b4592aa8e9d7ccfa189b43ecf505227cf1ab1
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-22 21:29:55 +0700

    Update approve_sql.py

--- FallenRobot/modules/sql/approve_sql.py
+++ FallenRobot/modules/sql/approve_sql.py
@@ -3,7 +3,7 @@ import threading
 from sqlalchemy import Column, String
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from FallenRobot.modules.sql import BASE, SESSION
+from AbingRobot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
