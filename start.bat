ef15705a6a09744d17d22ae7acc780699260a824
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-22 20:48:04 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Fallen Robot
-:: Enables virtual env mode and then starts Fallen
-env\scripts\activate.bat && py -m FallenRobot
+TITLE Abing Robot
+:: Enables virtual env mode and then starts Abing
+env\scripts\activate.bat && py -m AbingRobot
