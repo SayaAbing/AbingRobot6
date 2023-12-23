5e8bc048662634f701940e01bea24c49ea02cfb1
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-23 21:20:25 +0700

    Update nightmode.py

--- FallenRobot/modules/nightmode.py
+++ FallenRobot/modules/nightmode.py
@@ -2,10 +2,10 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from telethon import functions, types
 from telethon.tl.types import ChatBannedRights
 
-from FallenRobot import BOT_NAME
-from FallenRobot import telethn as tbot
-from FallenRobot.events import register
-from FallenRobot.modules.sql.night_mode_sql import (
+from AbingRobot import BOT_NAME
+from AbingRobot import telethn as tbot
+from AbingRobot.events import register
+from AbingRobot.modules.sql.night_mode_sql import (
     add_nightmode,
     get_all_chat_id,
     is_nightmode_indb,
