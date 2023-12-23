24af39441ce2d61f980a9d8e4072e371bc90d141
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-23 20:09:07 +0700

    Update backups.py

--- FallenRobot/modules/backups.py
+++ FallenRobot/modules/backups.py
@@ -7,22 +7,22 @@ from telegram import ParseMode
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler
 
-# from FallenRobot.modules.sql import warns_sql as warnssql
-import FallenRobot.modules.sql.blacklist_sql as blacklistsql
+# from AbingRobot.modules.sql import warns_sql as warnssql
+import AbingRobot.modules.sql.blacklist_sql as blacklistsql
 
-# from FallenRobot.modules.sql import cust_filters_sql as filtersql
-# import FallenRobot.modules.sql.welcome_sql as welcsql
-import FallenRobot.modules.sql.locks_sql as locksql
-import FallenRobot.modules.sql.notes_sql as sql
+# from AbingRobot.modules.sql import cust_filters_sql as filtersql
+# import AbingRobot.modules.sql.welcome_sql as welcsql
+import AbingRobot.modules.sql.locks_sql as locksql
+import AbingRobot.modules.sql.notes_sql as sql
 
-# from FallenRobot.modules.rules import get_rules
-import FallenRobot.modules.sql.rules_sql as rulessql
-from FallenRobot import EVENT_LOGS, LOGGER, OWNER_ID, SUPPORT_CHAT, dispatcher
-from FallenRobot.__main__ import DATA_IMPORT
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.helper_funcs.alternate import typing_action
-from FallenRobot.modules.helper_funcs.chat_status import user_admin
-from FallenRobot.modules.sql import disable_sql as disabledsql
+# from AbingRobot.modules.rules import get_rules
+import AbingRobot.modules.sql.rules_sql as rulessql
+from AbingRobot import EVENT_LOGS, LOGGER, OWNER_ID, SUPPORT_CHAT, dispatcher
+from AbingRobot.__main__ import DATA_IMPORT
+from AbingRobot.modules.connection import connected
+from AbingRobot.modules.helper_funcs.alternate import typing_action
+from AbingRobot.modules.helper_funcs.chat_status import user_admin
+from AbingRobot.modules.sql import disable_sql as disabledsql
 
 
 @user_admin
@@ -323,7 +323,7 @@ def export_data(update, context):
         },
     }
     baccinfo = json.dumps(backup, indent=4)
-    with open("FallenRobot{}.backup".format(chat_id), "w") as f:
+    with open("AbingRobot{}.backup".format(chat_id), "w") as f:
         f.write(str(baccinfo))
     context.bot.sendChatAction(current_chat_id, "upload_document")
     tgl = time.strftime("%H:%M:%S - %d/%m/%Y", time.localtime(time.time()))
@@ -339,15 +339,15 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("FallenRobot{}.backup".format(chat_id), "rb"),
-        caption="📤*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `FallenRobot-Backup` was specially made for notes 📚.".format(
+        document=open("AbingRobot{}.backup".format(chat_id), "rb"),
+        caption="📤*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `AbingRobot-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("FallenRobot{}.backup".format(chat_id))  # Cleaning file
+    os.remove("AbingRobot{}.backup".format(chat_id))  # Cleaning file
 
 
 # Temporary data
