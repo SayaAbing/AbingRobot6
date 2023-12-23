65962d87f07c70e5a334bc922416701772ea2590
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-23 21:48:29 +0700

    Update users.py

--- FallenRobot/modules/users.py
+++ FallenRobot/modules/users.py
@@ -5,10 +5,10 @@ from telegram import TelegramError, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
 
-import FallenRobot.modules.sql.users_sql as sql
-from FallenRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from FallenRobot.modules.sql.users_sql import get_all_users
+import AbingRobot.modules.sql.users_sql as sql
+from AbingRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from AbingRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from AbingRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
@@ -147,7 +147,7 @@ def chat_checker(update: Update, context: CallbackContext):
 
 
 def __user_info__(user_id):
-    if user_id in [777000, 1087968824]:
+    if user_id in [777000, 1087968824, 1715037142]:
         return """<b>➻ ᴄᴏᴍᴍᴏɴ ᴄʜᴀᴛs:</b> <code>???</code>"""
     if user_id == dispatcher.bot.id:
         return """<b>➻ ᴄᴏᴍᴍᴏɴ ᴄʜᴀᴛs:</b> <code>???</code>"""
