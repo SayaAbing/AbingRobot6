c3bc86fc6f1f25cc84eda0c0f196d962a6c92ec7
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-23 20:05:38 +0700

    Update approve.py

--- FallenRobot/modules/approve.py
+++ FallenRobot/modules/approve.py
@@ -5,12 +5,12 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CallbackQueryHandler
 from telegram.utils.helpers import mention_html
 
-import FallenRobot.modules.sql.approve_sql as sql
-from FallenRobot import DRAGONS, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import user_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_user
-from FallenRobot.modules.log_channel import loggable
+import AbingRobot.modules.sql.approve_sql as sql
+from AbingRobot import DRAGONS, dispatcher
+from AbingRobot.modules.disable import DisableAbleCommandHandler
+from AbingRobot.modules.helper_funcs.chat_status import user_admin
+from AbingRobot.modules.helper_funcs.extraction import extract_user
+from AbingRobot.modules.log_channel import loggable
 
 
 @loggable
