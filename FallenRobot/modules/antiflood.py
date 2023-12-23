5c88acb5a26a91cd8298bf0614ae916740cc699c
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-23 20:04:26 +0700

    Update antiflood.py

--- FallenRobot/modules/antiflood.py
+++ FallenRobot/modules/antiflood.py
@@ -12,19 +12,19 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html
 
-from FallenRobot import TIGERS, WOLVES, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.helper_funcs.alternate import send_message
-from FallenRobot.modules.helper_funcs.chat_status import (
+from AbingRobot import TIGERS, WOLVES, dispatcher
+from AbingRobot.modules.connection import connected
+from AbingRobot.modules.helper_funcs.alternate import send_message
+from AbingRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_user_admin,
     user_admin,
     user_admin_no_reply,
 )
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql import antiflood_sql as sql
-from FallenRobot.modules.sql.approve_sql import is_approved
+from AbingRobot.modules.helper_funcs.string_handling import extract_time
+from AbingRobot.modules.log_channel import loggable
+from AbingRobot.modules.sql import antiflood_sql as sql
+from AbingRobot.modules.sql.approve_sql import is_approved
 
 FLOOD_GROUP = 3
 
