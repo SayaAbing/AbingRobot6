8d56d5d0de9795ae1249a635e3b6bd443842aaa0
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-23 21:28:28 +0700

    Update remote_cmds.py

--- FallenRobot/modules/remote_cmds.py
+++ FallenRobot/modules/remote_cmds.py
@@ -2,15 +2,15 @@ from telegram import ChatPermissions, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
 
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import (
+from AbingRobot import LOGGER, dispatcher
+from AbingRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from FallenRobot.modules.helper_funcs.extraction import extract_user_and_text
-from FallenRobot.modules.helper_funcs.filters import CustomFilters
+from AbingRobot.modules.helper_funcs.extraction import extract_user_and_text
+from AbingRobot.modules.helper_funcs.filters import CustomFilters
 
 RBAN_ERRORS = {
     "User is an administrator of the chat",
