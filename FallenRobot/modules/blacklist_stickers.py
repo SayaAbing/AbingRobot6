c403dae41652f6a5452526ea28113c49c09a4f3e
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-23 20:14:13 +0700

    Update blacklist_stickers.py

--- FallenRobot/modules/blacklist_stickers.py
+++ FallenRobot/modules/blacklist_stickers.py
@@ -5,16 +5,16 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler
 from telegram.utils.helpers import mention_html, mention_markdown
 
-import FallenRobot.modules.sql.blsticker_sql as sql
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.warns import warn
+import AbingRobot.modules.sql.blsticker_sql as sql
+from AbingRobot import LOGGER, dispatcher
+from AbingRobot.modules.connection import connected
+from AbingRobot.modules.disable import DisableAbleCommandHandler
+from AbingRobot.modules.helper_funcs.alternate import send_message
+from AbingRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from AbingRobot.modules.helper_funcs.misc import split_message
+from AbingRobot.modules.helper_funcs.string_handling import extract_time
+from AbingRobot.modules.log_channel import loggable
+from AbingRobot.modules.warns import warn
 
 
 def blackliststicker(update: Update, context: CallbackContext):
