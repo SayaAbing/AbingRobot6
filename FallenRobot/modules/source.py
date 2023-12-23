a796282e05f0039ad6231bf809d7696036198a02
AbinG <101431998+SayaAbing@users.noreply.github.com>
2023-12-23 21:35:16 +0700

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from telegram import __version__ as o
 from telethon import __version__ as s
 
-from FallenRobot import BOT_NAME, BOT_USERNAME, OWNER_ID, START_IMG, pbot
+from AbingRobot import BOT_NAME, BOT_USERNAME, OWNER_ID, START_IMG, pbot
 
 
 @pbot.on_message(filters.command(["repo", "source"]))
@@ -17,7 +17,7 @@ async def repo(_, message: Message):
 
 ɪ ᴀᴍ [{BOT_NAME}](https://t.me/{BOT_USERNAME})**
 
-**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ :** 𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ :** ᴀʙɪɴɢ
 **» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
 **» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
